9910ff1a4ba03e3b1e4872284393248bdbb43638
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-16 22:26:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,2 @@
 
-
-INSERT INTO series (id, title, category, funding_goal, start_date, end_date) VALUES
-(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
-(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
-(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
-
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
